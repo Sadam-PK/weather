a05051e1419d02b784bdf5af29921cb8f5b455ed
sadam <sadamccr@gmail.com>
2022-12-29 15:59:40 +0500

    input and search added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <div className="col-mid-12">
         <div className="weatherBg">
           <h1 className='heading'>Weather App</h1>
+          <div className="d-grid col-4 mt-4">
+            <input type="text" className='form-control'/>
+            <button className='btn btn-primary my-3' type='button'>Search</button>
+          </div>
         </div>
       </div>
     </div>
